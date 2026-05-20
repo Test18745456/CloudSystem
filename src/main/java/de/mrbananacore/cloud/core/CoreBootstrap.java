3225f30c6f29ef3bda877130eb06470812753e9a
Test18745456 <lacydev@web.de>
2026-05-20 21:50:22 +0200

    Add CoreBootstrap class in de.mrbananacore.cloud.core package

---
+++ src/main/java/de/mrbananacore/cloud/core/CoreBootstrap.java
@@ -0,0 +1,7 @@
+package de.mrbananacore.cloud.core;
+
+public class CoreBootstrap {
+    public static void main(String[] args) {
+        System.out.println("CloudSystem Core gestartet");
+    }
+}
